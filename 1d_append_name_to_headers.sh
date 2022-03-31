b3744a492d03d2ab50bd08e6deaff2e9f8d42ff3
Atma M Ivancevic <atma.ivancevic@gmail.com>
2022-03-31 14:12:48 -0600

    Update 1d_append_name_to_headers.sh

--- 1d_append_name_to_headers.sh
+++ 1d_append_name_to_headers.sh
@@ -6,3 +6,4 @@
 # ELEMENT=<repeat_type> SPECIES=<species_name> bash 1d_append_name_to_headers.sh
 
 cat "$SPECIES"_"$ELEMENT"_combined.fasta | sed 's/^>/>'$SPECIES'_/g' > "$SPECIES"_"$ELEMENT"_final.fasta
+
