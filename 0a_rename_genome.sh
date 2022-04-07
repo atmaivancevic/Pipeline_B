41f8688d30b729309fe69fd6a4fabb57c10086ff
Atma M Ivancevic <atma.ivancevic@gmail.com>
2022-04-06 22:12:34 -0600

    Delete 0a_rename_genome.sh

--- 0a_rename_genome.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Rename genome file to include species name
-# Example usage: GENOME=<source_genome> SPECIES=<species_name> bash 0a_rename_genome.sh
-
-mv $GENOME "$SPECIES"_"$GENOME"
